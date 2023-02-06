186100dc8f5db6a4561b921877865c12b0ce62c6
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2023-02-06 20:30:57 +0700

    add flyweight

---
+++ src/main/java/structural/flyweight/Line.java
@@ -0,0 +1,22 @@
+package structural.flyweight;
+
+import java.awt.*;
+
+public class Line implements Shape {
+
+    public Line(){
+        System.out.println("Creating Line object");
+        //adding time delay
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics line, int x, int y, int width, int height, Color color) {
+        line.setColor(color);
+        line.drawLine(x, y, width, height);
+    }
+}
---
+++ src/main/java/structural/flyweight/Shape.java
@@ -0,0 +1,9 @@
+package structural.flyweight;
+
+import java.awt.*;
+
+public interface Shape {
+    public void draw(Graphics g, int x, int y, int width, int height,
+                     Color color);
+}
+
